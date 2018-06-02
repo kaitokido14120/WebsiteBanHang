3873e2886d60fef4e564ba0f43d425e689706db5
khai <kaitokido14120@gmail.com>
2018-06-02 09:05:09 +0700

    upload source website ban hang

---
+++ WebSiteBH/WebSiteBH/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebSiteBH.Startup))]
+namespace WebSiteBH
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
